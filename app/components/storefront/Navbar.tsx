f5949e8ad1dc881cb2a180e826e4f5fc89b010a3
liza811 <lizadjebara49@gmail.com>
2024-10-26 19:13:17 +0100

    logo

--- app/components/storefront/Navbar.tsx
+++ app/components/storefront/Navbar.tsx
@@ -36,7 +36,7 @@ export async function Navbar() {
           className="md:w-[180px] md:h-[180px] w-[120px] h-[120px] flex"
         >
           <Image
-            src={"/awramart-logo.png"}
+            src={"/awramart.png"}
             alt={"Logo"}
             width={200}
             height={200}
